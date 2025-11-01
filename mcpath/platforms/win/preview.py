f61b09a0edad73e5ae1511f67db972c26f2c5cdb
Legopitstop <1589lego@gmail.com>
2025-11-01 14:42:24 -0500

    Fix uwp path

--- mcpath/platforms/win/preview.py
+++ mcpath/platforms/win/preview.py
@@ -13,13 +13,14 @@ class WinPreviewEdition(Preview):
     def is_old(self, path: str) -> bool:
         return "Microsoft.MinecraftWindowsBeta" in path
 
-    def _get_game_uwp(self) -> Optional[str]:
+    def _get_game_uwp(self, *paths: str) -> Optional[str]:
         """
         Old path
         """
         p = path.expandvars(
             "%LOCALAPPDATA%\\Packages\\Microsoft.MinecraftWindowsBeta_8wekyb3d8bbwe\\LocalState\\games\\com.mojang"
         )
+        p = path.join(p, *paths)
         if path.isdir(p):
             return p
         return None
